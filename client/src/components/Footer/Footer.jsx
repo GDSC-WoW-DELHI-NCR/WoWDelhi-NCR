3ea97bb067fe9ca93847ae2631374b890830566f
chatbot-3 <sensationalatul10@gmail.com>
2024-03-02 22:55:21 +0530

    footer responsiveness

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,13 +1,17 @@
-
-import heart from "../../assets/images/heart.png"
+import heart from "../../assets/images/heart.png";
 
 function Footer() {
   return (
-      
-      <footer className="bg-g w-full text-center py-2 text-lg">
-        <p className="flex justify-center gap-3 text-[18px] md:text-[23px] font-bold">Made with <span><img src={heart} alt="" /></span> by TEAM WOW DELHI-NCR</p>
-      </footer>
-  )
+    <footer className="bg-g w-full text-center py-2 text-lg">
+      <p className="flex justify-center items-center gap-3 text-[12px] sm:text-[15px] md:text-[23px] font-bold">
+        Made with{" "}
+        <span>
+          <img src={heart} alt="" className="h-4 md:h-full" />
+        </span>{" "}
+        by TEAM WOW DELHI-NCR
+      </p>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
