59d26b56054e8673d0e2928d9400748cb53eec4a
geekamita <amitasingh5661@gmail.com>
2024-03-26 16:14:45 +0530

    Added Animation and Responsiveness in Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-import Hero from './components/Hero/Hero'
-import About from './components/About/About'
-import Contact from './components/Contact/Contact'
-import Footer from './components/Footer/Footer'
-import Registration from './components/Registration/Registration'
-import Sponsors from './components/Sponsors/Sponsors'
+import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import Registration from "./components/Registration/Registration";
+import Sponsors from "./components/Sponsors/Sponsors";
 import Navbar from "./components/Navbar/Navbar";
+import HomeAnim from "./components/HomeAnim/HomeAnim";
+import { useRef } from "react";
 
 function App() {
-
   return (
-    <div className="flex flex-col relative">
-      <Navbar />
-      <div>
-        <Hero />
-        <About />
-        <Sponsors/>
-        <Registration />
-        <Contact />
-        <Footer />
+    <div className="relative h-full">
+      <div className="absolute z-0">
+        <HomeAnim />
       </div>
+      <Navbar />
+      <Hero />
+      <About />
+      <Sponsors />
+      <Registration />
+      <Contact />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,21 +1,72 @@
-import logo from '../../assets/images/wow_logo.png';
+import { useState } from "react";
+import logo from "../../assets/images/wow_logo.png";
 import NavbarItem from "./NavbarItem";
 
 function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+
+  const hamburgerClicked = () => {
+    console.log("clicked");
+    setIsActive(!isActive);
+  };
+
   return (
-    <nav className="flex items-center justify-between w-full py-5 md:px-16 fixed top-0 z-10 bg-[#1E1E1E1E] ">
-      <img src={logo} alt="GDSC WOW Logo" />
-      <ul className="flex items-center md:space-x-10">
-        {NavbarItem.map((item, index) => {
-          return (
+    <div className="bg-background/60 fixed z-50 w-full backdrop-blur-md">
+      <nav className="fixed top-0 flex w-full items-center justify-between bg-[#1E1E1E1E] py-5 md:px-16 ">
+        <img src={logo} alt="GDSC WOW Logo" className="px-8" />
+        <ul className="hidden items-center md:flex md:space-x-10">
+          {NavbarItem.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className=" text-base text-[#F8F9FA] " href={item.href}>
+                  {item.name}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <button
+          className="z-40 flex flex-col items-start justify-evenly gap-1 px-8 md:hidden"
+          onClick={hamburgerClicked}
+        >
+          <span
+            className={`block h-[2.5px] w-6 origin-top-left rounded-full bg-white duration-300
+                  ${
+                    isActive
+                      ? "translate-x-0 translate-y-0 rotate-45 bg-green-400"
+                      : ""
+                  }`}
+          ></span>
+          <span
+            className={`block h-[2.5px] w-6 origin-center rounded-full bg-white duration-300
+                  ${isActive ? "translate-x-4 bg-green-400 opacity-0" : ""}`}
+          ></span>
+          <span
+            className={`block h-[2.5px] w-6 origin-bottom-left rounded-full bg-white duration-300
+                  ${
+                    isActive
+                      ? "translate-x-0 translate-y-[3px] -rotate-45 bg-green-400"
+                      : ""
+                  }`}
+          ></span>
+        </button>
+      </nav>
+      <div
+        className={`duration-400 bg-background/90 z-100 fixed left-2/4 hidden h-screen w-4/6 translate-x-full backdrop-blur-lg
+          ${isActive ? "block translate-x-0" : "hidden"}`}
+      >
+        <ul className="ml-8 mt-12 flex flex-col items-start gap-2">
+          {NavbarItem.map((item, index) => (
             <li key={index}>
-              <a className=" text-[#F8F9FA] text-base " href={item.href}>{item.name}</a>
+              <a className=" text-base text-[#F8F9FA] " href={item.href}>
+                {item.name}
+              </a>
             </li>
-          );
-        })}
-      </ul>
-    </nav>
-  )
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
